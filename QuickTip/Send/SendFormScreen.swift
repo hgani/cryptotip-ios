09d57f9425d201ceb3dbf405a4f89988b062c1cf
Hendrik Gani <hendrik.gani@gmail.com>
2018-05-26 10:17:09 +1000

    getting ready to support other fiat currencies

--- QuickTip/Send/SendFormScreen.swift
+++ QuickTip/Send/SendFormScreen.swift
@@ -3,14 +3,15 @@ import Eureka
 
 class SendFormScreen: GFormScreen {
     private var conversionRate: Float = 0.0
-    private let to: String
     private var section1 = Section()
     private var section2 = Section()
     
-    private let conversionLabel = GLabel().font(nil, size: 14)
+    private let fiatToEthLabel = GLabel().font(nil, size: 14)
+    private let ethToFiatLabel = GLabel().font(nil, size: 14)
+    
+    private let fiatCurrency = "AUD"
     
     private let conversionField = DataListRow(nil) { row in
-        row.title = "AUD Amount"
         row.options = ["5", "10", "20"]
     }
     
@@ -23,8 +24,8 @@ class SendFormScreen: GFormScreen {
     }
     
     init(to: String) {
-        self.to = to
         destinationField.value = to
+        conversionField.title = "\(fiatCurrency) Amount"
         super.init()
     }
     
@@ -47,7 +48,7 @@ class SendFormScreen: GFormScreen {
         section2.header = setupHeaderFooter() { view in
             view
                 .paddings(t: 10, l: 20, b: 10, r: 20)
-                .append(self.conversionLabel)
+                .append(GSplitPanel().width(.matchParent).withViews(self.fiatToEthLabel, self.ethToFiatLabel))
                 .end()
         }
         
@@ -58,10 +59,13 @@ class SendFormScreen: GFormScreen {
             row.title = "Review Transaction"
             }.onCellSelection { (cell, row) in
                 let values = self.values()
-                if let amount = Float(values["amount"] as! String) {
-                    let url = "ethereum:\(self.to)?amount=\(amount)"
+                if let amountText = values["amount"] as? String, let amount = Float(amountText), let recipient = self.destinationField.value {
+                    let url = "ethereum:\(recipient)?amount=\(amount)"
                     GLog.i("URL: \(url)")
                     self.launch.url(url)
+                }
+                else {
+                    self.launch.alert("Please enter amount and recipient")
                 }
         })
         
@@ -81,16 +85,20 @@ class SendFormScreen: GFormScreen {
     }
     
     override func onRefresh() {
-        self.conversionLabel.text("Retrieving ETH price ...")
+        _ = self.fiatToEthLabel.text("Retrieving ETH price ...")
 
-        _ = Rest.get(url: "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=AUD").execute(indicator: .null) { result in
-            if let rate = result["AUD"].float {
+        _ = Rest.get(url: "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=\(fiatCurrency)").execute(indicator: .null) { result in
+            if let rate = result[self.fiatCurrency].float {
                 self.conversionRate = 1 / rate
-                self.conversionLabel.text("1 AUD = \(self.conversionRate) ETH")
-                self.conversionField
-                    .intro("Specify the amount you want to send in AUD\n1 AUD = \(self.conversionRate) ETH")
+                _ = self.fiatToEthLabel.text("1 \(self.fiatCurrency) = \(self.conversionRate) ETH")
+                _ = self.ethToFiatLabel.text("1 ETH = \(rate) \(self.fiatCurrency)")
+                
+                _ = self.conversionField.intro("Specify the amount to send in \(self.fiatCurrency)\n1 \(self.fiatCurrency) = \(self.conversionRate) ETH")
                 self.tableView.reloadData()
                 return true
+            }
+            else {
+                _ = self.fiatToEthLabel.text("Failed!")
             }
             return false
         }
@@ -108,16 +116,5 @@ class SendFormScreen: GFormScreen {
             populate(view.clear())
         }
         return headerFooter
-    }
-    
-    func submit(code: String) {
-//        _ = Rest.post(path: "/", params: params).execute { result in
-//            if result["success"].boolValue {
-//                self.dismiss(animated: true, completion: nil)
-//                self.indicator.show(success: result["message"].stringValue)
-//                return true
-//            }
-//            return false
-//        }
     }
 }
