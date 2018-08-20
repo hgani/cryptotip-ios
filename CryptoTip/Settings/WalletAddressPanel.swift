280d3ff4affde02e1df9dae2d9581825da27ba03
Hendrik Gani <hendrik.gani@gmail.com>
2018-08-20 19:04:59 +1000

    display network

--- CryptoTip/Settings/WalletAddressPanel.swift
+++ CryptoTip/Settings/WalletAddressPanel.swift
@@ -8,19 +8,23 @@ class WalletAddressPanel: GVerticalPanel {
     init(nav: NavHelper) {
         self.nav = nav
         super.init()
-        
+                
         let addressView = GVerticalPanel()
             .append(GLabel().text("Your wallet address:"))
             .append(addressLabel)
         let content = GSplitPanel()
-            .paddings(t: 10, l: 10, b: 10, r: 10)
             .width(.matchParent)
             .withViews(
                 addressView,
                 GLabel().specs(.a).text("settings").onClick { _ in
                     nav.push(SettingsScreen())
             })
-        width(.matchParent).addView(content)
+        
+        self.width(.matchParent)
+            .paddings(t: 10, l: 10, b: 10, r: 10)
+            .append(content)
+            .append(GLabel().specs(.small).text("Network: \(EthNet.instance.simpleName)"), top: 10)
+            .done()
         
         reload()
     }
